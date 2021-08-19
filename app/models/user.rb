0fcd6bae92aed409be241404d3a788f2edfc909c
Tri <triphan921@gmail.com>
2021-08-18 18:13:55 -0700

    add validation to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+    has_secure_password
+    
+    validates :username, :password, presence: true
+    validates :password, confirmation: true
+    validates :username, uniqueness: { case_sensitive: false } 
 end
